68018d18828fb0a190296290471de238cfb8eb99
Nick Liu <synico@gmail.com>
2018-11-14 15:34:22 +0800

    add gitb.sh

---
+++ gitb.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "input:" $1 $2 "$3"
+optionstr="$1"
+if [ -n "$2" ];
+then
+  optiontstr=$optionstr" $2"
+fi
+
+for sub in `ls |grep -v hc`
+do
+  if [ -d $sub ]
+  then
+    echo $sub
+    git -C $PWD/$sub $1 $2 
+    echo "------------------------------------------"
+  fi
+done
